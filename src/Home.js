c1e957dc7bf5bb2b3cf8d5a02a2c27a2d042f4ab
PKG-7 <pavel.k.group@gmail.com>
2022-07-25 20:20:35 +0300

    finished

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
-export const Home = () => {
+import { useState } from "react";
 
-  const handleClick = () => console.log('Button clicked')
-  const handleClick2 = (obj, e) => console.log(obj + ' licked by a', e.type);
-  const handleNight = (animal, e) => console.log(animal + ' is going to RUN and ' + e.type);
+export const Home = () => {
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName('luigi');
+    setAge('30');
+  }
 
   return (
     <div className="home">
         <h2>Homepage</h2>
-    <button onClick={handleClick}>Click me</button>
-    <button onClick={(e) => handleClick2('Penis', e)}>Lick me again</button>
-    <button onClick={(e) => handleNight('Cat', e)}> Night has come</button>        
+        <p>{ name } is { age } years old</p>
+    <button onClick={handleClick}>Click me</button>      
     </div>
   )
 
