9c55f07a6461822c4b4271f1e76975297f92e2f8
PKG-7 <pavel.k.group@gmail.com>
2022-07-25 19:33:26 +0300

    finish + my practice

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,16 @@
 export const Home = () => {
 
-  const handleClick = (e) => console.log('Button clicked', e);
-
-  // удалить и повторить заново
-  const handleClickAgain = (obj, e) => {
-    console.log(`${obj} licked  ${e.type}`)
-  };
+  const handleClick = () => console.log('Button clicked')
+  const handleClick2 = (obj, e) => console.log(obj + ' licked by a', e.type);
+  const handleNight = (animal, e) => console.log(animal + ' is going to RUN and ' + e.type);
 
   return (
     <div className="home">
         <h2>Homepage</h2>
-        <button onClick={handleClick}>Click me</button>
-
-        <button onClick={(e) => handleClickAgain('Penis', e)}>Lick me again</button>
-        
+    <button onClick={handleClick}>Click me</button>
+    <button onClick={(e) => handleClick2('Penis', e)}>Lick me again</button>
+    <button onClick={(e) => handleNight('Cat', e)}> Night has come</button>        
     </div>
   )
-}
+
+};
