4c0e467ea8c5e3c69ebfd7a1944d73e4373dc7c0
PKG-7 <pavel.k.group@gmail.com>
2022-07-25 18:34:22 +0300

    finish

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 export const Home = () => {
+
+  const handleClick = (e) => console.log('Button clicked', e);
+
+  // удалить и повторить заново
+  const handleClickAgain = (obj, e) => {
+    console.log(`${obj} licked  ${e.type}`)
+  };
+
   return (
     <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+
+        <button onClick={(e) => handleClickAgain('Penis', e)}>Lick me again</button>
+        
     </div>
   )
 }
