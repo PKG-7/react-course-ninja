11b8d830ba63be719207e554efdb701073b11c06
PKG-7 <pavel.k.group@gmail.com>
2022-07-25 04:39:23 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
+import { Navbar } from './Navbar';
+import { Home } from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const person = { name: 'yoshi, age'}
-  const link = "https://yandex.ru/"
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p>{ 4*8 }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={ link }>Yandex</a>
-
-
+        <h1>
+          <Home />
+        </h1>
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <h1>The Dojo Block</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">NewBlog</a>
+        </div>
+    </nav>
+  )
+}
