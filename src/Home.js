61bd13f9915b68eabe6f939f4044c919e3ca0789
PKG-7 <pavel.k.group@gmail.com>
2022-07-26 23:50:34 +0300

    fininshed

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,24 @@ import { BlogList } from "./BlogList";
 export const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
     .then(res => {
+      if(!res.ok) {
+        throw Error('Couldnt fetch the data for that resourse');
+      }
       return res.json();
     })
     .then((data) => {
       setBlogs(data)
+      setIsPending(false)
+      setError(null)
+    })
+    .catch(err => {
+      setError(err.message);
       setIsPending(false)
     })
 
@@ -20,6 +29,7 @@ export const Home = () => {
 
   return (
     <div className="home">
+      { error && <div> { error } </div>}
       { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs = {blogs} title="All blogs!" />}
     </div>
