87d37b7285086fe8b54069c0a99fad2ad82992eb
PKG-7 <pavel.k.group@gmail.com>
2022-07-28 20:52:15 +0300

    done

--- src/create.js
+++ src/create.js
@@ -4,11 +4,22 @@ export default function Create() {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('mario')
+  const [isPending, setIsPending] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const blog = { title, body, author }
-    console.log(blog);
+
+    setIsPending(true)
+    
+    fetch('http://localhost:8000/blogs/',{
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added')
+      setIsPending(false)
+    })
   }
 
   return (
@@ -41,10 +52,12 @@ export default function Create() {
             <option value="mario">mario</option>
             <option value="yoshi">yoshi</option>
           </select>
+          {!isPending && <button>Add blog</button>}
+          {isPending && <button>Adding...</button>}
 
-          <button>Add blog</button>
+          
           <p>
-            {title} / { body }
+            {title}  { body }
           </p>
         </form>
     </div>
