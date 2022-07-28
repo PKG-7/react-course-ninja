c768cd7cb161953469ab80078ba3fbf94d89e9b4
PKG-7 <pavel.k.group@gmail.com>
2022-07-28 21:08:19 +0300

    done

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min"
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min"
 import { useFetch } from "./useFetch"
 
 
 export default function BlogDetails() {
     const { id } = useParams()
     const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/` + id)
+    const history = useHistory()
+
+    const handleClick = () => {
+      fetch('http://localhost:8000/blogs/'+ blog.id, {
+        method: 'DELETE'
+      }).then(() => {
+        history.push('/')
+      })
+    } 
 
   return (
     <div className="blog-details">
@@ -15,6 +24,7 @@ export default function BlogDetails() {
                 <h2>{ blog.title }</h2>
                 <p>Written by { blog.author }</p>
                 <p>{ blog.body }</p>
+                <button onClick={handleClick}>delete</button>
             </article>
         )}
     </div>
