bb70aca949a43fd0bcd59476c079fc1d5e6c6a88
PKG-7 <pavel.k.group@gmail.com>
2022-07-28 20:21:44 +0300

    finished

--- src/create.js
+++ src/create.js
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
 export default function Create() {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('mario')
+
+
   return (
     <div className="create">
         <h2>Add a a new blog</h2>
@@ -8,21 +15,32 @@ export default function Create() {
             placeholder="Your title"
             type="text"
             required
+            value={ title }
+            onChange={ (e) => setTitle(e.target.value) }
              />
 
           <label>Blog body:</label>
           <textarea
           placeholder="Your text"
-          required>
+          required
+          value={ body }
+          onChange={ (e) => setBody(e.target.value) }
+          >
           </textarea>
 
           <label>Blog author:</label>
-          <select>
+          <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          >
             <option value="mario">mario</option>
             <option value="yoshi">yoshi</option>
           </select>
 
           <button>Add blog</button>
+          <p>
+            {title} / { body }
+          </p>
         </form>
     </div>
   )
