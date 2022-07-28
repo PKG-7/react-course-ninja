ec7284a3865d276e6d04a8dad33ae1754822df02
PKG-7 <pavel.k.group@gmail.com>
2022-07-28 19:55:49 +0300

    works

--- src/create.js
+++ src/create.js
@@ -1,9 +1,29 @@
 export default function Create() {
   return (
     <div className="create">
-        <h2>
-            Add a a new blog
-        </h2>
+        <h2>Add a a new blog</h2>
+        <form>
+          <label>Blog title:</label>
+          <input 
+            placeholder="Your title"
+            type="text"
+            required
+             />
+
+          <label>Blog body:</label>
+          <textarea
+          placeholder="Your text"
+          required>
+          </textarea>
+
+          <label>Blog author:</label>
+          <select>
+            <option value="mario">mario</option>
+            <option value="yoshi">yoshi</option>
+          </select>
+
+          <button>Add blog</button>
+        </form>
     </div>
   )
 }
